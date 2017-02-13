695e1a35a30038b84ada5149fda1e730c371ee60
hymma200 <user200@Client.EvngClass.com>
2017-02-12 21:06:15 -0500

    Added my_strcpy Fun

--- string.c
+++ string.c
@@ -9,19 +9,29 @@ size_t my_strlen(const char *s)
 
    return (p - s);
 }
+char *my_strcpy(char *t, char *s)
+{
+   char *p = t;
+   while (*t++ = *s++)
+   ;
+
+   return p;
+}
 
 int main(void)
 {
    int i;
-   char *s[] = 
-   {
-      "Git tutorials",
+   char p1[32];	
+   char *s[] =  
+      {
+	"Git tutorials",
       "Tutorials Point"
-   };
-
+}	;
    for (i = 0; i < 2; ++i)
-      
+{      
    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+   printf("%s\n", my_strcpy(p1,"Hello,World !!!"));
 
    return 0;
+   }
 }
